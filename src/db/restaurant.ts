b97cacc73fd66deee04d06a8b6af1e6487793398
alexnainer <me@alexnainer.io>
2021-07-21 01:08:09 -0400

    Added routes and db queries for editing and removing items +returning more clear values from db

--- src/db/restaurant.ts
+++ src/db/restaurant.ts
@@ -1,27 +1,43 @@
 import db from "./index";
 
 const getRestaurantById = async (id: string) => {
-  const { rows } = await db.query(`SELECT * FROM restaurant WHERE id='${id}'`);
-  return rows;
+  const { rows } = await db.query(`
+    SELECT name FROM restaurant WHERE id='${id}'`);
+  return rows[0];
 };
 
 const getMenuByRestaurantId = async (id: string) => {
   const { rows } = await db.query(`
-  SELECT menu_category.name AS category_name, menu_item.name AS item_name, menu_item.id AS item_id,
-         menu_item.description, menu_item.price, menu_item.position
+  SELECT 
+    menu_category.name AS category_name,
+    menu_category.id AS menu_category_id,
+    menu_item.name AS menu_item_name, 
+    menu_item.id AS menu_item_id,
+    menu_item.description,
+    menu_item.price, 
+    menu_item.position
   FROM menu_item
   INNER JOIN menu_category ON menu_item.menu_category_id = menu_category.id
   WHERE menu_category.restaurant_id = '${id}'
   `);
 
   const data = {};
-  for (let i: number = 0; i < rows.length; i++) {
-    if (data[rows[i].category_name]) {
-      data[rows[i].category_name].items.push(rows[i]);
+  for (const menuItem of rows) {
+    const itemObj = {
+      menuItemName: menuItem.menu_item_name,
+      menuItemId: menuItem.menu_item_id,
+      position: menuItem.position,
+      menuItemPrice: menuItem.price,
+      description: menuItem.description,
+    };
+
+    if (data[menuItem.menu_category_id]) {
+      data[menuItem.menu_category_id].items.push(itemObj);
     } else {
-      data[rows[i].category_name] = {
-        name: rows[i].category_name,
-        items: [rows[i]],
+      data[menuItem.menu_category_id] = {
+        name: menuItem.category_name,
+        menuCategoryId: menuItem.menu_category_id,
+        items: [itemObj],
       };
     }
   }
